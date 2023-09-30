86fa2ffcb8e273e2055ba80f32a26a2e6c592ee0
Jonatas <jonatas.07022003@gmail.com>
2023-09-30 18:10:59 -0300

    Finalizando lógica do login

--- js/login.js
+++ js/login.js
@@ -4,15 +4,16 @@ const mensagemErro = document.getElementById('error')
 const botaoEntrar = document.getElementById('entrar')
 const botaoCadastrar = document.getElementById('cadastrar')
 
-function erroPreenchimento(){
-    usuario.classList.add('input-error')
-    senha.classList.add('input-error')
-    mensagemErro.style.display='block';
-    mensagemErro.innerText = "Preencha todos os campos"
-}
-
 botaoEntrar.addEventListener("click", () =>{
-    if(usuario.value == '' || senha.value == ''){
-        erroPreenchimento()
+    if(usuario.value == '' && senha.value == ''){
+        usuario.classList.add('input-error')
+        senha.classList.add('input-error')
+        mensagemErro.style.display='block';
     } 
+    
+    else{
+        usuario.classList.remove('input-error')
+        senha.classList.remove('input-error')
+        mensagemErro.style.display='none';
+    }
 })
