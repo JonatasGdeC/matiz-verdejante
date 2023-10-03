423e23d9a765fe4ca46cfcdf6a077fa88855c907
Jonatas <jonatas.07022003@gmail.com>
2023-10-03 19:58:51 -0300

    Atualizando sistema de cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,14 +1,19 @@
+//Máscara de input com o Jquery
 $("#data").mask('00/00/0000')
 $("#cpf").mask('000.000.000-00')
 $("#celular").mask('(00) 00000-0000')
 
+//Validação de preenchimento obrigatório com o Jquery
+
 $(".form").validate({
     rules:{
         nome:{
-            required:true
+            required:true,
+            minlength: 3,
         },
         sobrenome:{
-            required:true
+            required:true,
+            minlength: 3
         },
         email:{
             required:true
@@ -17,27 +22,38 @@ $(".form").validate({
             required:true
         },
         cpf:{
-            required:true
+            required:true,
+            minlength: 14,
         },
         celular:{
-            required:true
+            required:true,
+            minlength: 15,
         },
         senha:{
             required:true
         },
         repetirSenha:{
-            required:true
+            required:true,
+            equalTo:senha
         },
+        genero:{
+            required:true
+        }
     },
     messages:{
-        nome:'<span>Insira seu nome!</span>',
-        sobrenome:'<span>Insira seu sobrenome!</span>',
-        email:'<span>Insira seu email!</span>',
+        nome:'<span>Este campo é obrigatório!</span>',
+        sobrenome:'<span>Este campo é obrigatório!</span>',
+        email:'<span>Este campo é obrigatório!</span>',
         data:'<span>Insira sua data de nascimento!</span>',
         cpf:'<span>Insira seu CPF!</span>',
         celular:'<span>Insira seu número de celular!</span>',
         senha:'<span>Esse campo não pode estar vazio!</span>',
         repetirSenha:'<span>Repita a senha digitada!</span>',
+        genero:'<span>Selecione seu gênero!</span>'
     }
 })
+
+const form = document.getElementById('form')
+const cadastro = document.getElementById('criar')
+const nome = document.getElementById('name')
 
--- js/login.js
+++ js/login.js
@@ -1,16 +1,25 @@
+//Variáveis da página de login
 const usuario = document.getElementById('user')
 const senha = document.getElementById('password')
 const mensagemErro = document.getElementById('error')
 const botaoEntrar = document.getElementById('entrar')
 const botaoCadastrar = document.getElementById('cadastrar')
 
+function erroMensagem(){
+    usuario.classList.add('input-error')
+    senha.classList.add('input-error')
+    mensagemErro.style.display='block';
+}
+
+//Validação do sistema após o click do botão "Entrar"
 botaoEntrar.addEventListener("click", () =>{
-    if(usuario.value == '' && senha.value == ''){
-        usuario.classList.add('input-error')
-        senha.classList.add('input-error')
-        mensagemErro.style.display='block';
-    } 
-    
+    if(usuario.value == '' || senha.value == ''){
+        erroMensagem()
+        mensagemErro.innerText='Preencha todos os campos!'
+    } else if (usuario.value !== 'admin' || senha.value !== 'admin'){
+        erroMensagem()
+        mensagemErro.innerText='Usuário ou senha incorretos!'
+    }
     else{
         usuario.classList.remove('input-error')
         senha.classList.remove('input-error')
