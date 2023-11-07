ed71d559965b10276fbb22b4b81f94cf145d8704
geovanne <geovamelo4431@gmail.com>
2023-11-06 21:33:50 -0300

    Atualizando

--- js/novoProduto.js
+++ js/novoProduto.js
@@ -2,12 +2,56 @@ const createSpan = document.createElement('span')
 const regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/; //Regra de existência de caracteres especiais
 
 const nome = document.getElementById('nome')
+const marca = document.getElementById('marca')
+const categoria = document.getElementById('categoria')
+const genero = document.getElementById('genero')
+const descricao = document.getElementById('descricao')
 
-nome.addEventListener('keyup',()=>{
+var nomeCorreto = nome.addEventListener('keyup',()=>{
     if(regex.test(nome.value)){
         createSpan.textContent='Não escreva caracteres especiais!'
         nome.insertAdjacentElement('afterend', createSpan)
+    } else if (nome.value.length < 3){
+        createSpan.textContent='É necessário no mínimo 3 caracteres!'
+        nome.insertAdjacentElement('afterend', createSpan)
+    }else{
+        createSpan.remove()
+    }
+})
+
+marca.addEventListener('focusout', ()=>{
+    if(marca.value == ''){
+        createSpan.textContent='Adicione a marca do produto!'
+        marca.insertAdjacentElement('afterend', createSpan)
     } else{
         createSpan.remove()
     }
+})
+
+categoria.addEventListener('focusout', ()=>{
+    if(categoria.value == ''){
+        createSpan.textContent='Adicione a categoria do produto!'
+        categoria.insertAdjacentElement('afterend', createSpan)
+    } else{
+        createSpan.remove()
+    }
+})
+
+genero.addEventListener('focusout', ()=>{
+    if(genero.value == ''){
+        createSpan.textContent='Adicione o gênero do produto!'
+        genero.insertAdjacentElement('afterend', createSpan)
+    } else{
+        createSpan.remove()
+    }
+})
+
+
+var caracteresMin = 150
+
+descricao.addEventListener('keypress',()=>{
+    caracteresMin -=1
+    createSpan.textContent=`Digite pelo menos ${caracteresMin} caracteres!`
+    descricao.insertAdjacentElement('afterend', createSpan)
+
 })
